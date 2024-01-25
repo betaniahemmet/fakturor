c4eaf5a1a1f1330508c5088ada4b28688d33cdf8
henrik björserud <henrikbjorserud@gmail.com>
2024-01-25 13:26:32 +0100

    updated requirements. updated tree.

--- tree.py
+++ tree.py
@@ -1,9 +1,11 @@
+from app.config import fakturor_path
+
 from directory_tree import display_tree
 
 # Specify the root directory of your project
 
 directory_structure = display_tree(
-    "C:/Users/henri/Documents/code/fakturor", ignore_list=["venv"]
+    fakturor_path, ignore_list=["venv", "__pycache__"]
 )
 # Print the ASCII tree representation
 print(directory_structure)
