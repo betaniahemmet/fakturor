ae5461d19994d8bd1083bf12680e28d417e1cdd3
henrik björserud <henrikbjorserud@gmail.com>
2024-01-25 13:12:49 +0100

    Ran Black

--- tests/test_app.py
+++ tests/test_app.py
@@ -12,9 +12,8 @@ THEN check if the response is valid
 
 def test_home_page_get():
     flask_app = create_app()
-    flask_app.register_blueprint(main_blueprint, name='test_blueprint')
+    flask_app.register_blueprint(main_blueprint, name="test_blueprint")
 
     with flask_app.test_client() as test_client:
-        response = test_client.get('/')
+        response = test_client.get("/")
         assert response.status_code == 200
-        
--- tree.py
+++ tree.py
@@ -1,6 +1,9 @@
 from directory_tree import display_tree
+
 # Specify the root directory of your project
 
-directory_structure = display_tree('C:/Users/henri/Documents/code/fakturor', ignore_list = ['venv'])
+directory_structure = display_tree(
+    "C:/Users/henri/Documents/code/fakturor", ignore_list=["venv"]
+)
 # Print the ASCII tree representation
 print(directory_structure)
