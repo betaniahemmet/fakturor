92a3f7f2c5b3e04c403d4a8851315d6dd3ff07c1
henrik björserud <henrikbjorserud@gmail.com>
2025-03-06 20:28:49 +0100

    fixed paths to be handled better, updated waitress and flask

--- app/config.py
+++ app/config.py
@@ -1,17 +1,13 @@
+import os
 from pathlib import Path
 
-fakturor_path = Path(__file__).parents[1]  # The path to fakturor folder
+# Check environment variable to determine the base directory
+fakturor_path = Path(os.getenv('FAKTUROR_PATH', Path(__file__).parents[1]))  # Default to the parent folder if not set
 
-invoices_path = fakturor_path / "invoices"  # The path to invoices templates
-
-jobs_path = fakturor_path / "jobb"  # The path to where the invoices go when ready
-
-images_path = fakturor_path / "images"  # The path to the images for the invoices
-
-mag_path = fakturor_path / "orders"  # The path to orders
-
-test_temp_folder_path = (
-    fakturor_path / "test_temp_folder"
-)  # A folder to test test-functions on
+invoices_path = fakturor_path / "invoices"
+jobs_path = fakturor_path / "jobb"
+images_path = fakturor_path / "images"
+mag_path = fakturor_path / "orders"
+test_temp_folder_path = fakturor_path / "test_temp_folder"
 
 secret = "askimsviken"
